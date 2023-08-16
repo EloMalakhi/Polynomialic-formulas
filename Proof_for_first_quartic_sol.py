7f44c8bc9407e91d0887f2661d6e15ed2ff0c9a9
EloMalakhi <117231135+EloMalakhi@users.noreply.github.com>
2023-08-16 08:44:34 -0500

    Made a more accurate proof

--- Proof_for_first_quartic_sol.py
+++ Proof_for_first_quartic_sol.py
@@ -1,98 +1,87 @@
 import sympy
-a, b, c, d, e = sympy.symbols('a b c d e')
-M1, M2, M3, M4, M5, M6 = sympy.symbols('M1 M2 M3 M4 M5 M6')
-S1, S2, S3, S4, P1, P2, P3, P4, P5 = sympy.symbols('S1 S2 S3 S4 P1 P2 P3 P4 P5')
+a,b,c,d,e = sympy.symbols('a b c d e')
+P1, P2, P3, P4, P5 = sympy.symbols('P1 P2 P3 P4 P5')
+M3, M4, M5 = sympy.symbols('M3 M4 M5')
 
-p3_squared = P1**2 - 4*P4 - 4*P5
-p1 = P2 - b/(2*a)
-p2_squared = b*b/(4*a*a) - M3
-p4 = c/(2*a) - M3/2
-m3 = 2*c/(3*a) + M4/3 + M5/3
-p2_p5 = d/(2*a) - b*c/(12*a*a) + b*M4/(12*a) + b*M5/(12*a)
-p5_squared = P4**2 - e/a
-p4_squared = sympy.expand((c/(2*a) - M3/2)**2)
+x = P1/(4*a) + P3/(4*a)
+proof = sympy.expand(a*x**4 + b*x**3 + c*x**2 + d*x + e)
 
-x = (P1 + P3)/2
+print("f(x) = ax**4 + bx**3 + cx**2 + dx + e")
+print("")
+print("x = P1/(4*a) + P3/(4*a)")
+print("")
+print(f"Therefore f(x) = {proof}")
+print("")
 
-expanded = sympy.expand(a*x**4 + b*x**3 + c*x**2 + d*x + e)
-Doc1 = """x = (P1 + P3)/2
-f(x) = P1**4*a/16 + P1**3*P3*a/4 + P1**3*b/8 + 3*P1**2*P3**2*a/8 + 3*P1**2*P3*b/8 + P1**2*c/4 + P1*P3**3*a/4 + 3*P1*P3**2*b/8 + P1*P3*c/2 
-      + P1*d/2 + P3**4*a/16 + P3**3*b/8 + P3**2*c/4 + P3*d/2 + e
-root_coefficient = P1**3*a/4 + 3*P1**2*b/8 + P1*P3**2*a/4 + P1*c/2 + P3**2*b/8 + d/2
-simple_coefficient = P1**4*a/16 + P1**3*b/8 + 3*P1**2*P3**2*a/8 + P1**2*c/4 + 3*P1*P3**2*b/8 + P1*d/2 + P3**4*a/16 + P3**2*c/4 + e
-f(x) = ax^4 + bx^3 + cx^2 + dx + e
-f(x) = simple_coefficient + root_coefficient*P3
-
-P3 is the most complex root of x
-therefore it can't be neutralized by any group of terms, it must have a coefficient of 0
-P2*P5 can be proven to be (2*d/a - b*c/(3*a**2) + b*M4/(3*a) + b*M5/(3*a))/4 but we won't go into it now."""
-
-root_coefficient = P1**3*a/4 + 3*P1**2*b/8 + P1*P3**2*a/4 + P1*c/2 + P3**2*b/8 + d/2
-simple_coefficient = P1**4*a/16 + P1**3*b/8 + 3*P1**2*P3**2*a/8 + P1**2*c/4 + 3*P1*P3**2*b/8 + P1*d/2 + P3**4*a/16 + P3**2*c/4 + e
+Replacements = [(P3**2, P1**2 - 8*a*P4 - 8*a*P5),
+                (P3**3, P1**2*P3 - 8*a*P4*P3 - 8*a*P5*P3),
+                (P5**2, P4**2 - 4*a*e),
+                (P1, P2 - b),
+                (P2**2, b**2 - 4*a*M3),
+                (P2**3, b**2*P2 - 4*a*M3*P2),
+                (P4, c - M3),
+                (M3, 2*c/3 + M4/3 + M5/3)]
 
 
-root_coefficient_replacements = [(P3**2, p3_squared), 
-                                 (P1, p1), (P2**2, p2_squared), 
-                                 (P2**3, P2*p2_squared), 
-                                 (P4, p4), (M3, m3), 
-                                 (P2*P5, p2_p5)]
+print("enter . to see replacements")
+dot = input()
+if  dot == ".":
+    print("f(x) is going to refer to incremental replacements in the result of f(x)")
+    for i in Replacements:
+        print(f"replacing {i[0]} with {i[1]}")
+        proof = sympy.expand(proof.subs(i[0], i[1]))
+        print(f"    f(x) = {proof}")
+        input()
 
-simple_coefficient_replacements = [(P3**2, p3_squared),
-                                   (P1**4, P1**3*p1),
-                                   (P1**3, P1**2*p1),
-                                   (P2**2, p2_squared),
-                                   (P4, p4), (P1**2, P1*p1),
-                                   (P5**2, p5_squared),
-                                   (P2*P5, p2_p5),
-                                   (P1*P2*c/2, P2*p1*c/2), 
-                                   (P4**2, p4_squared),
-                                   (P2**2, p2_squared), 
-                                   (P1*b*c/(3*a), p1*b*c/(3*a)), 
-                                   (P1*P5, p1*P5), 
-                                   (P1*P2, p1*P2), 
-                                   (P2**2, p2_squared), 
-                                   (M3*P1*b/2, M3*p1*b/2), 
-                                   (M4*P1*b/6, M4*p1*b/6), 
-                                   (M3*P2*b/4, m3*P2*b/4), 
-                                   (M5*P1*b/6, M5*p1*b/6), 
-                                   (M3*b**2, m3*b**2), 
-                                   (P1*d, p1*d), 
-                                   (P2*P5, p2_p5), 
-                                   (M3*P5, m3*P5)]
+if dot != ".":
+    for i in Replacements:
+        proof = sympy.expand(proof.subs(i[0], i[1]))
+    print("\n\nHere is the result of f(x) after all replacements")
+    print(f"    f(x) = {proof}")
 
-print(Doc1)
-input("")
-for i in root_coefficient_replacements:
-    root_coefficient = sympy.expand(root_coefficient.subs([(i[0], i[1])]))
-    print(f"replacing {i[0]} with {i[1]}: ")
-    print(f"   {root_coefficient}")
-    input("")
+print(f"\nReplacing P3, P2, and P5 with 0")
+part1 = proof.subs([(P3, 0), (P2, 0), (P5, 0)])
+print(f"this is part 1: {part1}\n")
 
-print("SIMPLIFYING SIMPLE COEFFICIENT")
-for i in simple_coefficient_replacements:
-    simple_coefficient = sympy.expand(simple_coefficient.subs([(i[0], i[1])]))
-    print(f"replacing {i[0]} with {i[1]}: ")
-    print(f"   {simple_coefficient}")
-    input("")
+print(f"\nSubtracting part 1 and replacing P3 and P2 with 0")
+part2 = proof.subs([(P3, 0), (P2, 0)]) - part1
+print(f"this is part 2: {part2}")
 
-Doc2 = """for the formula to be proven this last part must be simplified to 0, assimilating it into parts gives:
-P2*(-M4*b/12 - M5*b/12 - d/2 + b*c/(12*a)) + P5*(-M4*a/3 - M5*a/3 - 2*c/3 + b**2/(4*a))
-we know that P2*P5 = d/(2*a) - b*c/(12*a**2) + b*M4/(12*a) + b*M5/(12*a)
-therefore -a*P2*P5 = -d/2 + b*c/(12*a) -  b*M4/12 - b*M5/12
+print(f"\nSubtracting part 1 and part 2 from f(x) and replacing P3 and P5 with 0")
+part3 = (sympy.expand(proof - part1 - part2)).subs([(P3, 0), (P5, 0)])
+print(f"this is part 3: {part3}")
 
-substituting this into the formula gives:
-P5*(-M4*a/3 - M5*a/3 - 2*c/3 + b**2/(4*a) - a*P2**2)
+print(f"\nSubtracting part 1, part 2, and part 3 from f(x) and replacing P3 with 0")
+part4 = (sympy.expand(proof - part1 - part2 - part3).subs([(P3, 0)]))
+print(f"this is part 4: {part4}")
 
-setting simple_coefficient to P5*(-M4*a/3 - M5*a/3 - 2*c/3 + b**2/(4*a) - a*P2**2)
-replacing P2**2 with b*b/(4*a*a) - M3
-replacing M3*P5*a with {m3*P5*a}"""
-simple_coefficient = sympy.expand(P5*(-M4*a/3 - M5*a/3 - 2*c/3 + b**2/(4*a) - a*P2**2))
-simple_coefficient = sympy.expand(simple_coefficient.subs([(P2**2, p2_squared)]))
-simple_coefficient = sympy.expand(simple_coefficient.subs([(M3*P5*a, m3*P5*a)]))
-print(Doc2)
-input('')
-print(simple_coefficient)
-# 0
+print(f"\nSubtracting part 1, part 2, part 3, and part 4 from f(x) gives")
+part5 = sympy.expand(proof - part1 - part2 - part3 - part4)
+print(f"{part5}")
+
+part2 = part2.subs([(P5, 1)])
+part3 = part3.subs([(P2, 1)])
+part5 = part5.subs([(P3, 1)])
+print(f"\nTherefore f(x) = {part1} +")
+print(f"                  P5*({part2}) +")
+print(f"                  P2*({part3}) +")
+print(f"                  {part4} +")
+print(f"                  P3*({part5})")
+
+print(f"\nP2*P5 = ±(2ad - bc/3 + bM4/3 + bM5/3)")
+print(f"The only way everything up to this point to be valid is for f(x) to be zero")
+print(f"f(x) will only be zero if every part here is 0")
+print(f"Every part will be zero if P2*P5 = (2ad - bc/3 + bM4/3 + bM5/3)")
+print("")
+print(f"But in reality if P2 is a square root of (b*b - 4*a*M3)")
+print(f"and P5 is a square root of (P4*P4 - 4*a*e)")
+print(f"The different square roots are seldom going to match (2ad - bc/3 + bM4/3 + bM5/3)")
+
+print(f"The formula works in theory if P2*P5 = (2ad - bc/3 + bM4/3 + bM5/3)")
+print(f"The formula works in practical application if the sign of P2*P5 correctly causes the result to be (2ad - bc/3 + bM4/3 + bM5/3)")
+
+
+
 
 
 
